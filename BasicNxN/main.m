8fa2dee31ed8f8c5a2a700434a78bf049f56f947
klumel8 <floris.brulleman@gmail.com>
2018-04-25 15:05:30 +0200

    Update main.m

--- BasicNxN/main.m
+++ BasicNxN/main.m
@@ -1,6 +1,6 @@
-clear all;
-%Particles in our model; compvec
-N = 3;
+clear all; close all;
+%Particles in our model;
+N = 5;
 
 level_of_awesomeness = 4;
 
@@ -10,31 +10,38 @@ defaultMass = 5*10^24;
 %Mass of each particle; to be replaced with random; to be replaced by
 %actual mass.
 Mass = linspace(1,N,N) * defaultMass;
+
 %make a mass combination vector similar as range vector (but then
 %multiplied instead of subtracted).
 G = 6.67408*10^-11;
 
 %Default range
-defaultRange = 10^6;
+defaultRange = 10^9;
 
 %Make the startposition parmeters (xyz)
 X = cos(linspace(1,N,N)/N*2*pi);
 Y = sin(linspace(1,N,N)/N*2*pi);
 Z = (linspace(1,N,N));
+
+%make the total position vector
 p = [X; Y; Z] * defaultRange;
 
 %Defualt speed
-defaultSpeed = 1*10^4;
+defaultSpeed = 4*10^2;
 
 %Make the startspeed parameters;
 Vx = cos(linspace(1,N,N)/N*2*pi + pi/2);
 Vy = sin(linspace(1,N,N)/N*2*pi + pi/2);
 Vz = linspace(1,N,N)*0;
+
+%total speed vector
 v  = [Vx; Vy; Vz] * defaultSpeed;
 
-M = 1/10;
-S = 1/10;
-T = 10000;
+%M = multiplier, (read code below), S = 'stepsize', T = 'total time'
+M = 10;
+S = 100;
+T = 10000000;
+
 %time step value
 step = S*M;
 
@@ -43,11 +50,12 @@ tVal = T*M;
 
 %pos = zeros(3*N,round(tVal/step));
 
-figure;
-hold on;
-for i=1:N
-    plot(p(1,i),p(2,i),'*');
-end
+%some old plot code
+% figure;
+% hold on;
+% for i=1:N
+%     plot(p(1,i),p(2,i),'*');
+% end
 
 %index will later be used to keep track of iterations in order to make a
 %plot vector
@@ -59,15 +67,33 @@ for t = 0:step:tVal
     %later were gonna make some bounds on speed and range, this is needed.
     vOud = v;
     
+    %read fo.m first, but keeps track of whether there was a collision.
     [~,c] = fo(p,Mass,G,N);
+    
+    %#BUG will crash if multiple collisions in one timestep
+    
+    %check if the collision vector is empty
     if max(max(c)) > 0
+       
+       %find indices of collided particles
        find(c);
+       
+       %re-rank the collision indexes
        indices = mod(find(c),N);
        indices = (indices==0)*N + indices
+       
+       
        %ik ga uit van compleet inelastisch.
+       %new position is mass centre
        p(:,indices(1)) = (Mass(indices(1))*p(:,indices(1)) + Mass(indices(2))*p(:,indices(2)))/sum(Mass(indices));
+       
+       %use momentum fomulae for new speed
        v(:,indices(1)) = (Mass(indices(1))*v(:,indices(1)) + Mass(indices(2))*v(:,indices(2)))/sum(Mass(indices));
+       
+       %new mass is um of the masses
        Mass(indices(1)) = sum(Mass(indices));
+       
+       %speed of old particle is 0
        v(:,indices(2)) = [0;0;0];
        Mass(indices(2)) = 0;
     end
@@ -109,6 +135,23 @@ for t = 0:step:tVal
     %liever niet vol maken want dan heb je nullen
     pos(:,index) = reshape(p,[],1);
 end
-for i=0:N-1
-    plot(pos(3*i+1,:),pos(3*i+2,:));
+for i=1:N
+    xPos(i,:) = pos(3*(i-1)+1,:);
+    yPos(i,:) = pos(3*(i-1)+2,:);
+    zPos(i,:) = pos(3*(i-1)+3,:);
 end
+
+hold off
+%make the axis stable
+minX = min(min(xPos));
+maxX = max(max(xPos));
+minY = min(min(yPos));
+maxY = max(max(yPos));
+minZ = min(min(zPos));
+maxZ = max(max(zPos));
+for t=1:100:size(xPos,2);
+    plot3(xPos(:,t),yPos(:,t),zPos(:,t),'*');
+    axis([minX maxX minY maxY minZ maxZ]);
+    drawnow
+end
+
