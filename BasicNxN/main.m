c7d1aff852a0ba1941948553bd44ae625c1c210e
koloneltedi <sashaivlev@hotmail.com>
2018-05-01 09:14:17 +0200

    Update main.m

--- BasicNxN/main.m
+++ BasicNxN/main.m
@@ -1,56 +1,53 @@
-clear all; close all;
-%Particles in our model;
-N = 5;
+clear; close all;
+%Particles in our model; combvec
+N = 1000;
 
-level_of_awesomeness = 4;
+level_of_awesomeness = 4; %Which Numerical Integration Method is used
 
 %default mass (earth):
 defaultMass = 5*10^24;
 
 %Mass of each particle; to be replaced with random; to be replaced by
 %actual mass.
-Mass = linspace(1,N,N) * defaultMass;
-Mass = reshape(Mass,[1,3]); %Used for the matrix multiplication in fo
-
+Mass = ones(1,N)* defaultMass;%linspace(1,N,N) * defaultMass;
+Mass = reshape(Mass,[1,N]); %Used for the matrix multiplication in fo
 %make a mass combination vector similar as range vector (but then
 %multiplied instead of subtracted).
 G = 6.67408*10^-11;
 
 %Default range
-defaultRange = 10^9;
+defaultRange = 10^6;
 
-%Make the startposition parmeters (xyz)
-X = cos(linspace(1,N,N)/N*2*pi);
-Y = sin(linspace(1,N,N)/N*2*pi);
+%Make the startposition parmeters (xyz) %Equidistributed over x
+X = cos(linspace(1,N,N)/N*2*pi); 
+Y = sin(linspace(1,N,N)/N*2*pi); 
 Z = (linspace(1,N,N));
-
-%make the total position vector
 p = [X; Y; Z] * defaultRange;
 
-%Defualt speed
-defaultSpeed = 4*10^2;
+%Default speed
+defaultSpeed = 1*10^4;
 
 %Make the startspeed parameters;
 Vx = cos(linspace(1,N,N)/N*2*pi + pi/2);
 Vy = sin(linspace(1,N,N)/N*2*pi + pi/2);
 Vz = linspace(1,N,N)*0;
-
-%total speed vector
 v  = [Vx; Vy; Vz] * defaultSpeed;
 
-% dt = 'stepsize', T = 'total time'
-dt = 100;
-T = 10000000;
 
+%time dt value
+dt = 1;
+
+%Total time duration
+T = 100;
 
 %pos = zeros(3*N,round(T/dt));
 
-%some old plot code
-% figure;
-% hold on;
-% for i=1:N
-%     plot(p(1,i),p(2,i),'*');
-% end
+
+figure;
+hold on;
+
+plot(p(1,:),p(2,:),'*');
+
 
 %index will later be used to keep track of iterations in order to make a
 %plot vector
@@ -62,33 +59,15 @@ for t = 0:dt:T
     %later were gonna make some bounds on speed and range, this is needed.
     vOud = v;
     
-    %read fo.m first, but keeps track of whether there was a collision.
-    [~,c] = fo(p,Mass,G,N);
-    
-    %#BUG will crash if multiple collisions in one timestep
-    
-    %check if the collision vector is empty
+    c = col(p,N);
     if max(max(c)) > 0
-       
-       %find indices of collided particles
        find(c);
-       
-       %re-rank the collision indexes
        indices = mod(find(c),N);
        indices = (indices==0)*N + indices
-       
-       
        %ik ga uit van compleet inelastisch.
-       %new position is mass centre
        p(:,indices(1)) = (Mass(indices(1))*p(:,indices(1)) + Mass(indices(2))*p(:,indices(2)))/sum(Mass(indices));
-       
-       %use momentum fomulae for new speed
        v(:,indices(1)) = (Mass(indices(1))*v(:,indices(1)) + Mass(indices(2))*v(:,indices(2)))/sum(Mass(indices));
-       
-       %new mass is um of the masses
        Mass(indices(1)) = sum(Mass(indices));
-       
-       %speed of old particle is 0
        v(:,indices(2)) = [0;0;0];
        Mass(indices(2)) = 0;
     end
@@ -97,25 +76,25 @@ for t = 0:dt:T
     
     if level_of_awesomeness == 1
         %first order (newton forward)
-        a = fo(p,Mass,G,N);
+        a = acc(p,Mass,G,N);
         p = p + v*dt + a * dt^2 / 2;
         v = v + a * dt;
     elseif level_of_awesomeness == 2
         %Runge Kutta 2 conserves angular momentum?
-        k1 = dt^2*permute(fo(p + (1/2)*dt*v,Mass,G,N),[3,2,1]);
+        k1 = dt^2*permute(acc(p + (1/2)*dt*v,Mass,G,N),[3,2,1]);
         p = p + v*dt + k1;
         v = v + k1/dt;
     elseif level_of_awesomeness == 4
         %Runge Kutta 4
-        k1 = dt^2*permute(fo(p,Mass,G,N),[3,2,1]);
-        k2 = dt^2*permute(fo(p + 0.5*dt*v + 1/8*k1,Mass,G,N),[3,2,1]);
-        k3 = dt^2*permute(fo(p + dt*v + .5*k2,Mass,G,N),[3,2,1]);
+        k1 = dt^2*permute(acc(p,Mass,G,N),[3,2,1]);
+        k2 = dt^2*permute(acc(p + 0.5*dt*v + 1/8*k1,Mass,G,N),[3,2,1]);
+        k3 = dt^2*permute(acc(p + dt*v + .5*k2,Mass,G,N),[3,2,1]);
         p = p + v*dt + 1/6*(k1+2*k2);
         v = v + 1/(6*dt)*(k1+4*k2+k3);
     end
     
     %als de snelheid te snel groter wordt stop dan het programma
-    if norm(v(:,:)) > 20*norm(vOud(:,:));
+    if norm(v(:,:)) > 20*norm(vOud(:,:))
         disp('Too fast')
         break
     end
@@ -129,24 +108,11 @@ for t = 0:dt:T
     
     %liever niet vol maken want dan heb je nullen
     pos(:,index) = reshape(p,[],1);
-end
-for i=1:N
-    xPos(i,:) = pos(3*(i-1)+1,:);
-    yPos(i,:) = pos(3*(i-1)+2,:);
-    zPos(i,:) = pos(3*(i-1)+3,:);
+
+
+
 end
 
-hold off
-%make the axis stable
-minX = min(min(xPos));
-maxX = max(max(xPos));
-minY = min(min(yPos));
-maxY = max(max(yPos));
-minZ = min(min(zPos));
-maxZ = max(max(zPos));
-for t=1:100:size(xPos,2);
-    plot3(xPos(:,t),yPos(:,t),zPos(:,t),'*');
-    axis([minX maxX minY maxY minZ maxZ]);
-    drawnow
+for i=0:N-1
+    plot3(pos(3*i+1,:),pos(3*i+2,:),pos(3*i+3,:));
 end
-
