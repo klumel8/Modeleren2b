d33b6a1f7c2813761831f68e11546726a364955b
nanleij <38724800+nanleij@users.noreply.github.com>
2018-05-01 11:30:24 +0200

    documentation in functions

--- BasicNxN/COM.m
+++ BasicNxN/COM.m
@@ -2,6 +2,11 @@
 function [CM] = COM(Mass,p)
 %COM Summary of this function goes here
 %   Calculate the centre of mass
+%input arguments:
+%   Mass    : (1xN) mass vector
+%   p       : (3xN) position vector
+%output arguments:
+%   CM      : (double) 
     CM = nansum(repmat(Mass, [3 1]).*p,2)/sum(Mass);
 end
 
