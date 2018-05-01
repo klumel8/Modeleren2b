8f317a26dffc2df89f50bae7ab0edd0b4778169f
koloneltedi <sashaivlev@hotmail.com>
2018-05-01 12:00:14 +0200

    Colision
    Use new MATLAB addition

--- BasicNxN/col.m
+++ BasicNxN/col.m
@@ -3,14 +3,12 @@ function Collision = col(p, Mass, N)
     
     %check if they collide.
     
-        
     %check if particles collide
     %make the radius of the particle dependent on size
     planetSize = Mass.^(1/3)/(4*pi);
  
     %do some things to get the linear combination of all added ranges
-    [comb1,comb2] = meshgrid(planetSize,planetSize);
-    planetCombi = comb1+comb2;
+    planetCombi = planetSize+planetSize';
     
     %check whether they collide
     Collision = (R < planetCombi).*R>0;
