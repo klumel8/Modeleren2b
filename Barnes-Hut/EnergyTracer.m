891778c85103576e0b4ed68730ed52731cd64e51
nanleij <38724800+nanleij@users.noreply.github.com>
2018-05-09 14:01:27 +0200

    Merge branch 'Barnes_Hut' (not including main)

---
+++ Barnes-Hut/EnergyTracer.m
@@ -0,0 +1,31 @@
+function [kin,pot] = EnergyTracer(p,N,v,Mass,G)
+%ENERGYTRACER calculates the kinetic and the potential energy of the given
+%particles
+%input arguments:
+%   p       : (3xN) position vector
+%   N       : (scalar) number of particles
+%   v       : (3xN) velocity vector
+%   Mass    : (1xN) mass vector
+%   G       : (scalar) gravitational constant
+%output arguments:
+%   kin     : (scalar) total kinetic energy
+%   pot     : (scalar) total potential energy
+%required functions(non-standard):
+%   dispVec
+
+    %first get the distances of each particle to the rest
+    [~,R] = dispVec(p,N);
+    
+    %use the standard formulae for gravitational energy and use r*2 becuase of the symmetry of the R matrix each distance (1 to 2 and 2 to 1) gets counted twice
+    pot = -G*(Mass'*Mass)./(R*2);
+    
+    %filter al the divide by zero potentials
+    pot(pot==-Inf) = 0;
+    pot(pot==Inf) = 0;
+    
+    %add them up
+    pot = nansum(nansum(pot));
+    
+    %use the standard formulea for kinetic energy
+    kin = nansum(0.5*Mass.*(v(1,:).^2 + v(2,:).^2 + v(3,:).^2)); 
+end
---
+++ Barnes-Hut/acc.m
@@ -0,0 +1,42 @@
+function a = acc(p, Mass, G, N)
+% Calculate the acceleration of all the particles based on the
+% gravitational force
+%input arguments:
+%   p       : (3xN) position matrix
+%   Mass    : (1xN) mass vector
+%   G       : (scalar) gravitational constant
+%   N       : (scalar)number of particles
+%output arguments:
+%   a       : (3xN) acceleration matrix
+%required functions(non-standard):
+%   dispVec
+    
+    %get the distances between the particles
+    [D,R] = dispVec(p,N);
+    
+    %make the mass product;
+    massProd = Mass'*Mass; %To make it faster
+
+    
+    %Make a simple mathematical expression for everything distance related,
+    %later to be used in the force formulae.
+    temp= repmat(R, [1,1,3]);
+    %gpuTemp = gpuArray(temp);
+    %gpuD = gpuArray(D);
+    %gpuDisp = gpuD./(gpuTemp.*gpuTemp.*gpuTemp);
+    
+    dispComp = D./(temp.^3);
+    
+    %calculate the force in each direction (x,y,z).
+    %no minus sign because somewhere I misplaced a minus sign (dont worry
+    %I anticipated this)
+    forceComp = G*repmat(massProd, [1,1,3]).*dispComp;
+    
+    %calculate the Force of each point on eachother  
+    F = nansum(forceComp,1);
+    
+    %calculate the acceleration in each direction.
+    a = F./repmat(Mass,[1,1,3]);
+    a = permute(a,[3,2,1]);
+end
+
